ae4ff74d77af682fb853a93e0ec66abb6c390b7f
Judicaël Anouma <judicael.anouma@appitforme.com>
2012-01-19 18:54:45 +0100

    Changed demo resources order

--- example/jsfiddle/demo.js
+++ example/jsfiddle/demo.js
@@ -1,4 +1,6 @@
-$("#coverflow").flickrCoverflow("526aaaa5cbca4f64991e80ea2c67c1e1", "12143321@N03", {
-    size: "small",
-    renderIn3D: true
+$(function(){
+	$("#coverflow").flickrCoverflow("526aaaa5cbca4f64991e80ea2c67c1e1", "12143321@N03", {
+    	size: "small",
+    	renderIn3D: true
+	});
 });
