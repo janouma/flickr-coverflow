96c163cb5292e845b743e814ca42c227901f51cd
Judicaël Anouma <janouma@gmail.com>
2015-11-29 14:26:58 +0100

    Refined test example

--- src/js/app.js
+++ src/js/app.js
@@ -11,5 +11,12 @@ new FlickrCoverflow({
 	apiKey: "526aaaa5cbca4f64991e80ea2c67c1e1",
 	user: "12143321@N03",
 	container: document.getElementById("flickr-coverflow-bis"),
+	"3d": false
+});
+
+new FlickrCoverflow({
+	apiKey: "526aaaa5cbca4f64991e80ea2c67c1e1",
+	user: "12143321@N03",
+	container: document.getElementById("flickr-coverflow-third"),
 	"3d": true
 });
