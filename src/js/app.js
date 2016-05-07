8df2a13958c2675f6b917e5274771cef93e9ac58
Judicaël Anouma <janouma@gmail.com>
2016-05-07 18:19:02 +0200

    Split app loading from app initialization

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,27 @@
 import FlickrCoverflow from 'flickr-coverflow'
 
-FlickrCoverflow.logLevel = 'debug'
+export default () => {
+  FlickrCoverflow.logLevel = 'debug'
 
-new FlickrCoverflow({
-  apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
-  user: '12143321@N03',
-  container: document.getElementById('flickr-coverflow'),
-  '3d': true
-})
+  new FlickrCoverflow({
+    apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
+    user: '12143321@N03',
+    container: document.getElementById('flickr-coverflow'),
+    '3d': true
+  })
 
-new FlickrCoverflow({
-  apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
-  user: '12143321@N03',
-  container: document.getElementById('flickr-coverflow-bis'),
-  '3d': false
-})
+  new FlickrCoverflow({
+    apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
+    user: '12143321@N03',
+    container: document.getElementById('flickr-coverflow-bis'),
+    '3d': false
+  })
 
-new FlickrCoverflow({
-  apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
-  user: '12143321@N03',
-  container: document.getElementById('flickr-coverflow-third'),
-  '3d': true
-})
+  new FlickrCoverflow({
+    apiKey: '526aaaa5cbca4f64991e80ea2c67c1e1',
+    user: '12143321@N03',
+    container: document.getElementById('flickr-coverflow-third'),
+    '3d': true
+  })
+}
 
